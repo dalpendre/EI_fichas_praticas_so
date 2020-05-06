a6181664acf9d7b7ae0521e9d43874dd0f1e0c3c
Diogo Alpendre <37402115+dalpendre@users.noreply.github.com>
2020-05-06 21:04:18 +0100

    Ficha 7
    Figura 1 eliminada

--- Ficha Prática 7/labs/lab1/main.c
+++ Ficha Prática 7/labs/lab1/main.c
@@ -5,7 +5,8 @@
 #define MAX 100
 #define ERR_IO 1
 
-int main(void) {
+int main(void) 
+{
 	FILE *fptr = NULL;
 	char buffer[MAX], *text;
 
@@ -14,13 +15,17 @@ int main(void) {
 		ERROR(ERR_IO, "Erro ao abrir o ficheiro para acrescimo");
 	
 	text = fgets(buffer, MAX, stdin);
-	while (text != NULL && strcmp(buffer, "fim\n") != 0) {
-		if (fputs(buffer, fptr) == EOF) { 
+	while (text != NULL && strcmp(buffer, "fim\n") != 0) 
+	{
+		if (fputs(buffer, fptr) == EOF) 
+		{ 
 			WARNING("Erro de escrita\n"); 
 			break;
 		}
+		
 		text = fgets(buffer, MAX, stdin); 
 	}
+
 	fclose(fptr); 
 	return 0;
 }
